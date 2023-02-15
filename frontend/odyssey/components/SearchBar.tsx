51daeaf5813823b8e30ee0aa8679026373d8e385
Jacquelyn (Jax) Powell <jacquelyneleanorpowell@gmail.com>
2023-02-14 19:45:43 -0800

    updated search bar and fixed jitters

--- frontend/odyssey/components/SearchBar.tsx
+++ frontend/odyssey/components/SearchBar.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import {TouchableOpacity, StyleSheet, Text, ViewStyle, TextStyle, ScrollView, View, Image, SafeAreaView, ImageBackground} from 'react-native'
+import {TouchableOpacity, TextInput, StyleSheet, Dimensions, Text, ViewStyle, TextStyle, ScrollView, View, Image, SafeAreaView, ImageBackground} from 'react-native'
 import { ProgressBar, Searchbar} from 'react-native-paper';
 import { AntDesign } from '@expo/vector-icons'; 
 
@@ -15,61 +15,38 @@ const SearchBar = ({style, textStyle, label, onPress}: ButtonProps) => {
     const onChangeSearch = query => setSearchQuery(query);
 
     return (
-        <View style={styles.rowContainer}>
-            <Searchbar style={styles.searchBar}
-                        placeholder="Search"
-                                onChangeText={onChangeSearch}
-                                value={searchQuery}
-                            />
-                            <TouchableOpacity style={[styles.button, style]} onPress={onPress}>
-                                <Text style={[styles.text, textStyle]}>{label}</Text>
-                            </TouchableOpacity>
-                            <AntDesign style={{marginLeft: "-11%"}} name="right" size={24} color="white" onPress={onPress}/>
-                    </View>
+        <View style={styles.inputContainer}>
+            <AntDesign name="search1" size={24}/>
+            <TextInput style={styles.searchBar}/>
+            <TouchableOpacity onPress={onPress} style={styles.icon}>
+                <AntDesign name="right" size={24} color="white"/>
+            </TouchableOpacity>
+        </View>
+
+
     )
 }
 
 const styles = StyleSheet.create({
-    button: {
-        // flex: 2,
-        backgroundColor: '#194260',
-        width: '17%',
-        marginLeft: '78%',
-        height: '60%',
-        borderRadius: 10,
-        // marginRight: 50,
-        // position: 'absolute'
+    inputContainer: { 
+        flexDirection: 'row', 
+        alignSelf: 'center', 
+        alignItems: 'center', 
+        justifyContent: 'center',
+        backgroundColor: "#f4f4f4",
+        width:  "80%",
+        marginTop: "5%",
+      },
+    icon: {
+        alignSelf: 'flex-end',
+        color: '194260',
+        backgroundColor: "#194260",
     },
-    text: {
-        textAlign: 'center',
-        color: 'white',
+    searchBar: {
+        width: "90%",
         fontSize: 20,
-    },
-    searchBar:{
-        // flex: 1,
-        marginLeft: 50,
-        // marginRight: 100,
-        width: '117%',
-        height: '64%',
-        marginTop: 30,
-        color: '#929292',
-        borderTopLeftRadius: 10,
-        borderTopRightRadius: 0,
-        borderBottomRightRadius: 0,
-        borderBottomLeftRadius: 10,
-        position: 'absolute'
-
-    },
-    rowContainer:{
-        flexDirection: 'row',
-        flexWrap: 'wrap',
-        alignItems: 'center',
-        position: 'absolute',
-        // backgroundColor:'#FFFFFF'
-        
-        
+        color: '#FFFFFF',
     }
-    
 })
 
 
