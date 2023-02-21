e21cda019b374ef54fea500f9eebf02e3f006964
Jacquelyn (Jax) Powell <jacquelyneleanorpowell@gmail.com>
2023-02-21 11:00:58 -0800

    changed background

---
+++ frontend/odyssey/components/SelectCalendar.tsx
@@ -0,0 +1,70 @@
+import {useState} from "react";
+import { StyleSheet, View, Text } from "react-native";
+import moment from "moment";
+import DateRangePicker from "react-native-daterange-picker";
+import React from "react";
+
+// export default class App extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       startDate: null,
+//       endDate: null,
+//       displayedDate: moment(),
+//     };
+//   }
+
+//   setDates = (dates) => {
+//     this.setState({
+//       ...dates,
+//     });
+//   };
+
+//   render() {
+//     const { startDate, endDate, displayedDate } = this.state;
+//     return (
+//       <View style={styles.container}>
+//         <DateRangePicker
+//           onChange={this.setDates}
+//           endDate={endDate}
+//           startDate={startDate}
+//           displayedDate={displayedDate}
+//           range
+//         >
+//           <Text>Click me!</Text>
+//         </DateRangePicker>
+//       </View>
+//     );
+//   }
+// }
+
+const SelectCalendar = () => {
+  const [startDate, setStartDate] = useState(null)
+  const [endDate, setEndDate] = useState(null)
+  const [displayedDate, setDisplayedDate] = useState(moment())
+
+  return (
+    <View style={styles.container}>
+         <DateRangePicker
+          onChange={(dates) => console.log(JSON.stringify(dates))}
+          endDate={endDate}
+          startDate={startDate}
+          displayedDate={displayedDate}
+          range
+        >
+          <Text>Click me!</Text>
+        </DateRangePicker>
+      </View>
+  )
+}
+
+export default SelectCalendar;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
