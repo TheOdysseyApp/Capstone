166a32bd661649d0ee63b927a095894e4cda1cf0
mary-kassab <kassabm@uci.edu>
2023-02-24 17:16:41 -0800

    Created and Deployed GraphQL API
    Created "User" Schema with fields: Id, fullname, created, username, and description

--- frontend/odyssey/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ frontend/odyssey/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "odyssey": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
   "auth": {
     "odysseye60f8fa0": {
       "AppClientID": "string",
