453f159f43724e5078140edee919e9c40ff5cf46
mary-kassab <kassabm@uci.edu>
2023-03-02 12:20:53 -0800

    User schema and storing user data to DynamoDB
    Storing user account info as user in DynamoDB when account is created

--- frontend/odyssey/App.tsx
+++ frontend/odyssey/App.tsx
@@ -1,6 +1,6 @@
 import { AppNavigator } from './navigation/app-navigator';
 import { Amplify } from 'aws-amplify'
-import awsconfig from './amplify/.config/aws-exports'
+import awsconfig from './src/aws-exports.js'
 
 Amplify.configure(awsconfig)
 
