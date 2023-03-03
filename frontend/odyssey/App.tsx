cb105c3d0a8698ab6a502fa9465560b181e14c20
jessica-gama <jgama1@uci.edu>
2023-03-02 16:56:00 -0800

    Built home screen

--- frontend/odyssey/App.tsx
+++ frontend/odyssey/App.tsx
@@ -1,6 +1,6 @@
 import { AppNavigator } from './navigation/app-navigator';
 import { Amplify } from 'aws-amplify'
-import awsconfig from './aws-exports'
+import awsconfig from './amplify/.config/aws-exports'
 
 Amplify.configure(awsconfig)
 
