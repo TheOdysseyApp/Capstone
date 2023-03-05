c77d6efac3fb56e356bcd9ce915091c9653c20c6
sgloveland <seangloveland@gmail.com>
2023-03-05 14:44:09 -0800

    Finished initial MST config

--- frontend/odyssey/mobx-models/root-store/root-store.ts
+++ frontend/odyssey/mobx-models/root-store/root-store.ts
@@ -1,9 +1,20 @@
 import {useMemo} from 'react'
 import {types} from 'mobx-state-tree'
+import { QuestionnaireStoreModel } from '../questionnaire-store/questionnaire.store';
 
-export const RootStore = types.model({
-
-})
+export const RootStore = types
+    .model("RootStore")
+    .props({
+        questionnaireStore: types.optional(
+            types.late(() => QuestionnaireStoreModel),
+            {}
+        )
+    })
+    .actions((self) => ({
+        resetData() {
+            self.questionnaireStore.resetStore();
+        }
+    }))
 
 let _store;
 export const useStores = () => {
