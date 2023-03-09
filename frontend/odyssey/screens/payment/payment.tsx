34cea2326a95de40b92fa1a6c6737119161510ec
ashlynpark <ashlynpark6@gmail.com>
2023-03-09 14:03:04 -0800

    integrated stripe checkout session

--- frontend/odyssey/screens/payment/payment.tsx
+++ frontend/odyssey/screens/payment/payment.tsx
@@ -1,18 +1,32 @@
-import { StripeProvider } from '@stripe/stripe-react-native';
-import { SafeAreaView } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, SafeAreaView } from 'react-native';
+import { WebView } from 'react-native-webview';
 
-function Payment() {
+const Payment = () => {
+  const [sessionURL, setSessionURL] = useState(null);
+  //var sessionURL = ""; //TODO: replace with payment cancelled screen url
+  useEffect(() => {
+    const fetchData = async () => {
+      fetch('https://ajk17v39ec.execute-api.us-west-1.amazonaws.com/test/checkout', {
+        method: 'GET'
+      }).then(response => response.json())
+        .then(data => {
+          setSessionURL(data.body);
+          console.log(data.statusCode);
+          console.log(sessionURL);
+        })
+        .catch(error=>console.error(error))
+    };
+
+    fetchData();
+  }, []);
+
   return (
-    <SafeAreaView>
-        <StripeProvider
-        publishableKey="pk_test_TYooMQauvdEDq54NiTphI7jx"
-        urlScheme="your-url-scheme" // required for 3D Secure and bank redirects
-        merchantIdentifier="merchant.com.{{YOUR_APP_NAME}}" // required for Apple Pay
-        >
-        
-        </StripeProvider>
-    </SafeAreaView>
+      <WebView
+        source = {{ uri: sessionURL }}
+        style={{ flex:1}}
+      />
   );
-}
+};
 
 export default Payment;
